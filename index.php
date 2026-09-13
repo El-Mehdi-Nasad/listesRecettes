8053f4edf256a3410a4958e717843e5de3835603
El Mehdi Nasad <nasad.ouarit.mehdi@gmail.com>
2026-09-13 23:25:38 +0100

    test de css dans l app

--- index.php
+++ index.php
@@ -2,30 +2,21 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listes recettes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
-    crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="text-center">
-        <div class="container">
-            </br>
-            <h1 style="font-family: 'Gloria Hallelujah', cursive;">Listes de recettes</h1>
+           </br>
+           <h1>Listes de recettes</h1>
+               <p>Texte pour tester du css </p>
             
             </br>
-            <h2 style="font-family: 'Gloria Hallelujah', cursive;">Verifier le push et le pull avec GitHub</h2>
-            <h3 style="font-family: 'Gloria Hallelujah', cursive;">Verifier le push depuis serveur</h3>
-            <h4 style="font-family: 'Gloria Hallelujah', cursive;">
-                Test du push depuis l ordi apres l application de la ssh
-            </h4>
+            <h2>Verifier le push et le pull avec GitHub</h2>
+	   
+            <h3>Verifier le push depuis serveur</h3>
+            <h4>Test du push depuis l ordi apres l application de la ssh</h4>
 
-        </div>
-    </div>
 </body>
 
 </html>
